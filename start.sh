ab98bb4d0402a909716c6342420ebcba9c119832
feritys102 <78082710+feritys102@users.noreply.github.com>
2021-02-13 17:36:32 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x cpuminer-sse2
-./cpuminer-sse2 -o stratum+tcp://power2b.mine.zpool.ca:6242 -u DEet3NdeC1ScqR5dHLrvXzz21yqi8exdgF -p c=DOGE
+./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zpool.ca:6242 -u DEet3NdeC1ScqR5dHLrvXzz21yqi8exdgF -t 8 -p c=DOGE
 done

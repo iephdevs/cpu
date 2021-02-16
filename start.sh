027679ddbad181e6479ab3cda6104a36b7d793aa
Ieph Devs <42347975+iephdevs@users.noreply.github.com>
2021-02-16 12:16:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x cpuminer-sse2
-./cpuminer-sse2 -a cryptonight -o pool.supportxmr.com:7777 -u 83zBwWS9NazMDCBnVdrALPd47ez6f8ES8Zo8GrWVg6MjAvZEc1uBECQXeHp7ejmh3jeMNmHyZpep4NgUhZyvDQZNHaLvKJF -p c=xmr
+./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u D8vaqNQMiL8RdVGXfMYJScwjg8Sjp85Y1y -p c=DOGE
 done
